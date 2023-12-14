06b5cf1c85691339c0d0b9059c82d8fef40820ae
Sukhmandeep Kaur <144360826+Sukhmandeep04@users.noreply.github.com>
2023-12-13 23:05:56 -0500

    initial changes

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -4,12 +4,13 @@ const handleOperation = async (model, operation, action, req, res) => {
   try {
     const result = await model[operation](...action);
     if (!result) {
-      res.status(404).json({ error: `${model.modelName} not found for ${operation}` });
-    } else {
-      res.json(result);
+      const notFoundError = `${model.modelName} not found for ${operation}`;
+      return res.status(404).json({ error: notFoundError });
     }
+    res.json(result);
   } catch (err) {
-    res.status(500).json({ error: `Error ${action.length > 1 ? 'updating' : 'fetching'} ${model.modelName}` });
+    const errorMessage = `Error ${action.length > 1 ? 'updating' : 'fetching'} ${model.modelName}`;
+    res.status(500).json({ error: errorMessage });
   }
 };
 
@@ -22,13 +23,17 @@ exports.getAllProjects = async (req, res) => {
 };
 
 exports.getProjectById = async (req, res) => {
-  await handleOperation(Project, 'findById', [req.params.projectId], req, res);
+  const { projectId } = req.params;
+  await handleOperation(Project, 'findById', [projectId], req, res);
 };
 
 exports.updateProjectById = async (req, res) => {
-  await handleOperation(Project, 'findByIdAndUpdate', [req.params.projectId, req.body, { new: true }], req, res);
+  const { projectId, body } = req.params;
+  const action = [projectId, body, { new: true }];
+  await handleOperation(Project, 'findByIdAndUpdate', action, req, res);
 };
 
 exports.deleteProjectById = async (req, res) => {
-  await handleOperation(Project, 'findByIdAndDelete', [req.params.projectId], req, res);
+  const { projectId } = req.params;
+  await handleOperation(Project, 'findByIdAndDelete', [projectId], req, res);
 };
