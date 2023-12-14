10ac2ac5475f45f39aee843758cf2eaf9683c01e
Sukhmandeep Kaur <144360826+Sukhmandeep04@users.noreply.github.com>
2023-12-13 22:57:15 -0500

    initial changes

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,39 +1,34 @@
 const Project = require('../../models/project');
 
-const handleProjectOperation = async (operation, action, req, res) => {
+const handleOperation = async (model, operation, action, req, res) => {
   try {
-    const result = await Project[operation](...action);
+    const result = await model[operation](...action);
     if (!result) {
-      res.status(404).json({ error: `Project not found for ${operation}` });
+      res.status(404).json({ error: `${model.modelName} not found for ${operation}` });
     } else {
       res.json(result);
     }
   } catch (err) {
-    res.status(500).json({ error: `Error ${action.length > 1 ? 'updating' : 'fetching'} project` });
+    res.status(500).json({ error: `Error ${action.length > 1 ? 'updating' : 'fetching'} ${model.modelName}` });
   }
 };
 
-// Create a new project
 exports.createProject = async (req, res) => {
-  await handleProjectOperation('create', [req.body], req, res);
+  await handleOperation(Project, 'create', [req.body], req, res);
 };
 
-// Get all projects
 exports.getAllProjects = async (req, res) => {
-  await handleProjectOperation('find', [], req, res);
+  await handleOperation(Project, 'find', [], req, res);
 };
 
-// Get a specific project by ID
 exports.getProjectById = async (req, res) => {
-  await handleProjectOperation('findById', [req.params.projectId], req, res);
+  await handleOperation(Project, 'findById', [req.params.projectId], req, res);
 };
 
-// Update a project by ID
 exports.updateProjectById = async (req, res) => {
-  await handleProjectOperation('findByIdAndUpdate', [req.params.projectId, req.body, { new: true }], req, res);
+  await handleOperation(Project, 'findByIdAndUpdate', [req.params.projectId, req.body, { new: true }], req, res);
 };
 
-// Delete a project by ID
 exports.deleteProjectById = async (req, res) => {
-  await handleProjectOperation('findByIdAndDelete', [req.params.projectId], req, res);
+  await handleOperation(Project, 'findByIdAndDelete', [req.params.projectId], req, res);
 };
