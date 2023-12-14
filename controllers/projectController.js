feddbc82d133b53e20aaacd4033e176889e39237
Sukhmandeep Kaur <144360826+Sukhmandeep04@users.noreply.github.com>
2023-12-13 22:54:09 -0500

    initial changes

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,63 +1,39 @@
 const Project = require('../../models/project');
 
+const handleProjectOperation = async (operation, action, req, res) => {
+  try {
+    const result = await Project[operation](...action);
+    if (!result) {
+      res.status(404).json({ error: `Project not found for ${operation}` });
+    } else {
+      res.json(result);
+    }
+  } catch (err) {
+    res.status(500).json({ error: `Error ${action.length > 1 ? 'updating' : 'fetching'} project` });
+  }
+};
+
 // Create a new project
 exports.createProject = async (req, res) => {
-  try {
-    const newProject = await Project.create(req.body);
-    res.json(newProject);
-  } catch (err) {
-    res.status(500).json({ error: 'Error creating project' });
-  }
+  await handleProjectOperation('create', [req.body], req, res);
 };
 
 // Get all projects
 exports.getAllProjects = async (req, res) => {
-  try {
-    const projects = await Project.find();
-    res.json(projects);
-  } catch (err) {
-    res.status(500).json({ error: 'Error fetching projects' });
-  }
+  await handleProjectOperation('find', [], req, res);
 };
 
 // Get a specific project by ID
 exports.getProjectById = async (req, res) => {
-  try {
-    const project = await Project.findById(req.params.projectId);
-    if (!project) {
-      res.status(404).json({ error: 'Project not found' });
-    } else {
-      res.json(project);
-    }
-  } catch (err) {
-    res.status(500).json({ error: 'Error fetching project' });
-  }
+  await handleProjectOperation('findById', [req.params.projectId], req, res);
 };
 
 // Update a project by ID
 exports.updateProjectById = async (req, res) => {
-  try {
-    const updatedProject = await Project.findByIdAndUpdate(req.params.projectId, req.body, { new: true });
-    if (!updatedProject) {
-      res.status(404).json({ error: 'Project not found' });
-    } else {
-      res.json(updatedProject);
-    }
-  } catch (err) {
-    res.status(500).json({ error: 'Error updating project' });
-  }
+  await handleProjectOperation('findByIdAndUpdate', [req.params.projectId, req.body, { new: true }], req, res);
 };
 
 // Delete a project by ID
 exports.deleteProjectById = async (req, res) => {
-  try {
-    const deletedProject = await Project.findByIdAndDelete(req.params.projectId);
-    if (!deletedProject) {
-      res.status(404).json({ error: 'Project not found' });
-    } else {
-      res.json(deletedProject);
-    }
-  } catch (err) {
-    res.status(500).json({ error: 'Error deleting project' });
-  }
+  await handleProjectOperation('findByIdAndDelete', [req.params.projectId], req, res);
 };
